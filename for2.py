2b755f34c6d6d9603ab107953b4934b526eadee7
midwarapu <midwarapu@itmsys.net>
2026-09-21 10:47:53 +0530

    new file for2 Exercise

---
+++ for2.py
@@ -0,0 +1,100 @@
+# for i in range(10,0,-1):
+#     print(i)
+
+# for i in range(1,11):
+#     print(i)
+
+# # 3rd question:
+
+# for i in range(1,21):
+#     if i % 2 == 0:
+#         print(i)
+
+# # 4th question:
+
+# for i in range(1,21):
+#     if i % 2 != 0:
+#         print(i)
+
+# # 5th question :
+
+# count = 0 
+# for i in range(1,11):
+#     count = count + i 
+
+# print(count)
+
+# # 6th question :
+# even_count = 0
+
+# for i in range(1,21):
+#     if i %2 == 0:
+#         even_count = even_count + i 
+
+# print(even_count)
+
+# # 7th question :
+
+# odd_count = 0
+
+# for i in range(1,21):
+#     if i % 2 !=0:
+#         odd_count = odd_count + i
+
+# print(odd_count)
+
+# # 8th question:
+
+# five_divisible_count = 0
+
+# for i in range(1,51):
+#     if i % 5 ==0:
+#         five_divisible_count = five_divisible_count + 1
+
+# print(five_divisible_count)
+
+# 9th question:
+
+# num = int(input("Enter a number to get that number multiplication : "))
+
+# for j in range(1,21):
+#     value = j * num
+#     print(value)
+
+# 10th question:
+
+# for i in range(1,11):
+#     value = i * i
+#     print(value)
+
+# 11th question:
+
+# for i in range(1,11):
+#     value = i * i * i 
+#     print(value)
+
+# 12 th question:
+
+# count1 = 0 
+
+# for i in range(1,101):
+#     if i % 3 ==0 and i % 5 ==0:
+#         count1 = count1 + 1
+
+# print(count1) 
+
+# largest_value = 0
+
+# numbers = [12, 45, 7, 89, 23, 56, 91, 34]
+
+# for i in numbers:
+#     if i > largest_value:
+#         largest_value = i
+
+# print(largest_value) 
+
+
+
+
+
+
