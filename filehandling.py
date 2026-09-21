e8a8b2c96264a71141cc23b9c4997d9d84dca7f9
midwarapu <tgopinath385@gmail.com>
2026-09-21 16:01:11 +0530

    File Handling practice files

---
+++ filehandling.py
@@ -0,0 +1,32 @@
+# Roadmap_file = open("C:\\python\\DataStructures\\roadmap.txt","x")
+
+# file = open("roadmap.txt","w")
+# file.write("Pyhton roadmap \n 1. first learn the python basics \n 2. learn data structures and algorithms")
+# file.close()
+
+# file = open("roadmap.txt","r")
+# print(file.read())
+
+# file = open("roadmap.txt","a")
+# file.write("3. Learn the sql database and concepts like joins and others")
+# file.close()
+
+# file = open("roadmap.txt","r")
+# data = file.read()
+# data = data.replace("algorithms3." ,"algorithms\n 3. ")
+# file = open("roadmap.txt","w")
+# file.write(data)
+# file.close()
+
+# with open("roadmap.txt","r") as file:
+#     data = file.read()
+#     print(data)
+
+#     data = data.replace("3.  Learn the sql database and concepts like joins and others" , "3. Learn the Sql with Advance topics")
+#     with open("roadmap.txt","w") as file:
+#         file.write(data)
+
+# with open("roadmap.txt","r") as file:
+#     data = file.read()
+#     print(data)
+
